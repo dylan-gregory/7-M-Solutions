7b17bb35b551519152bc1f05ecf975cf3138ff6a
dylan-gregory <jdylangregory@gmail.com>
2017-07-24 11:30:54 -0400

    Added NativeBase for thumbnail component, sprucing up list item components

--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-import { Text, Image, View } from 'react-native';
+import { Image, View } from 'react-native';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import { Actions } from 'react-native-router-flux';
+import { CardItem, Left, Body, Thumbnail, Right, Text } from 'native-base';
 import { Card, CardSection, Button } from './common';
 
 class ContactItem extends Component {
   render(){
     return (
       <Card>
-        <CardSection>
-          <Text style={styles.textStyle}>Photo?/Client Name</Text>
-        </CardSection>
+        <CardItem>
+          <Left>
+            <Thumbnail source={require('../images/7MS-logo-black.png')} />
+            <Text style={styles.textStyle}>Client Name</Text>
+          </Left>
+
+        </CardItem>
       </Card>
     );
   };
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
-import { Text, Image, View } from 'react-native';
+import { Image, View } from 'react-native';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import { Actions } from 'react-native-router-flux';
+import { CardItem, Left, Body, Thumbnail, Right, Text } from 'native-base';
 import { Card, CardSection, Button } from './common';
 
 class TaskItem extends Component {
   render(){
     return (
       <Card>
-        <CardSection>
-          <Text style={styles.textStyle}>Task title/description</Text>
+        <CardItem>
+          <Left>
+            <Text style={styles.textStyle}>Task title/description</Text>
+          </Left>
+
           <Text style={styles.textStyle}>Client Name</Text>
-        </CardSection>
-        <CardSection>
-          <Text style={styles.textStyle}>Phone #</Text>
-          <Text style={styles.textStyle}>Due Date</Text>
-          <Text style={styles.textStyle}>Last touch</Text>
-        </CardSection>
+        </CardItem>
+
+        <CardItem>
+          <Left>
+            <Text style={styles.textStyle}>Due Date</Text>
+          </Left>
+          <Right>
+            <Text style={styles.textStyle}>Last touch</Text>
+          </Right>
+        </CardItem>
+
+        <CardItem>
+          <Left>
+            <Text note style={styles.textStyle}>Phone #</Text>
+          </Left>
+
+        </CardItem>
       </Card>
     );
   };
